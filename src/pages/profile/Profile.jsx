6749bcf319f7bb3338110c2331d62eef6b37fca7
Melissa <melissamichaud0319@gmail.com>
2023-09-20 22:16:01 -0400

    Finished frontend - resposive, profile, home, etc

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,68 @@
 import "./profile.scss";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts";
 
 const Profile = () => {
-  return <div className="profile">Profile</div>;
+  return (
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://content.wepik.com/statics/7136137/preview-page0.jpg"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/woman-light-skin-tone.png"
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Mel</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>USA</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>codewithmel.com</span>
+              </div>
+            </div>
+            <button>follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
